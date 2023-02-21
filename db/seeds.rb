6850d6a1b34d031937f1ca3998e490a0092d723d
Milagros Lasarte <lasartemilagros@gmail.com>
2023-02-21 15:51:02 +0100

    Completed without CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  title = Faker::Book.title
+  content = Faker::Quote.famous_last_words
+  Article.create(title: title, content: content)
+end
